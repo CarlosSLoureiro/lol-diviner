0c189c1d575b951fe531a86b2590171173a17125
Carlos Loureiro <carlos.loureiro@pebmed.com.br>
2024-07-15 11:57:11 -0300

    feat: define algoritmo de busca

--- src/vectorStore.ts
+++ src/vectorStore.ts
@@ -4,4 +4,8 @@ import { embeddings } from "./embeddings";
 export const vectorStore = new Milvus(embeddings, {
     collectionName: "champions",
     url: "http://localhost:19530",
+    indexCreateOptions: {
+        index_type: "HNSW",
+        metric_type: "COSINE",
+    },
 });
