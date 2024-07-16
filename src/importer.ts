6fcb6a6f01ce488102ec3985bbebc91c593810fe
Carlos Loureiro <carlos.loureiro@pebmed.com.br>
2024-07-16 13:35:42 -0300

    feat: inclui ícone do personagem no banco

--- src/importer.ts
+++ src/importer.ts
@@ -5,10 +5,11 @@ interface Champion {
   name: string;
   title: string;
   lore: string;
+  icon: string;
 };
 
 export const importer = async () => {
-  console.log("Wait a moment while we import the champions data...");
+  console.log("Wait a moment while the champions data are imported...");
 
   const request = await fetch("http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions.json");
   const response = await request.json();
@@ -19,8 +20,9 @@ export const importer = async () => {
     return new Document({
       pageContent: champion.lore,
       metadata: {
-        characterName: champion.name,
-        characterTitle: champion.title,
+        name: champion.name,
+        title: champion.title,
+        icon: champion.icon,
       },
     });
   });
